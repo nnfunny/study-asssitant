2ccb147bfe65474861a1421773a269d3c932fed7
Nam Nguyen <nguyenvietnam2401@gmail.com>
2019-08-02 11:29:29 +1000

    The first commit for a new project]

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "jquery/src/jquery.js"
+import "popper.js/dist/popper.js"
+import "bootstrap/dist/js/bootstrap.min.js"
 import Vue from 'vue'
 import App from './App.vue'
+
 
 Vue.config.productionTip = false
 
