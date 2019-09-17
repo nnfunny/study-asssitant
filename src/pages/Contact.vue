1c0f9123731692e9c9f1a44f31cb3eb3eee87213
Nam Nguyen <nguyenvietnam2401@gmail.com>
2019-09-18 09:40:18 +1000

    Update contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -29,6 +29,16 @@
           </div>
         </div>
       </div>
+      <div class="contact-link">
+        <button>Submit</button>
+        <div class="media">
+          <a href="https://www.linkedin.com/in/nam-nguyen-cs/" target=”_blank”><i class="fab fa-linkedin"></i></a>
+          <a href="https://github.com/nnfunny" target=”_blank”><i class="fab fa-github"></i></a>
+          <a href="https://www.instagram.com/nam.viet.nguyen/" target=”_blank”><i class="fab fa-instagram"></i></a>
+          <a href="https://twitter.com/namnguyenDev" target=”_blank”><i class="fab fa-twitter"></i></a>
+          <a href="https://www.facebook.com/vietnam.nguyen.186" target=”_blank”><i class="fab fa-facebook"></i></a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -50,11 +60,35 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   height: 60vh;
 
+  
+  margin-top: 10px;
+}
+
+.contact-link button {
   border: 0.5px solid rgba(178, 201, 178, 0.568);
   box-shadow: 1px 1px 1px 1px rgba(145, 153, 145, 0.459);
-  margin-top: 10px;
+  border-radius: 5px;
+  width: 100px;
+  height: 30px;
+  font-weight: bold;
+  outline: none;
+}
+
+.contact-link {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.media a {
+  transform: scale(2);
+  cursor: pointer;
+  margin: 30px;
+  color: rgb(120, 177, 120)
 }
 
 .email,
